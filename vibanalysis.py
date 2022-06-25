ab4fb732fdf4a0b29db536eee127cf589216b7f3
EduAnacleto <eduardo.statistic@gmail.com>
2022-06-25 02:01:20 -0300

    getParameters included

--- vibanalysis.py
+++ vibanalysis.py
@@ -1,45 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import os
 from sklearn.cluster import KMeans
-
-def create_directory(folder_name):
-
-    #Create folder if not exists
-    isExist = os.path.exists(path + str(num_coleta) + "/" + folder_name)
-    if not isExist:
-        os.makedirs(path + str(num_coleta) + "/" + folder_name)
-
-
-def DFT(y, SAMPLE_RATE):
-    Y = np.abs(fft(y))
-    N = len(Y)
-    freq = fftfreq(N, 1.0 / SAMPLE_RATE)
-    N = N//2
-    return F[:N], Y[:N]
-
-def DFT_normalized(y, SAMPLE_RATE):
-    Y = np.abs(fft(y))
-    N = len(Y)
-    F = fftfreq(N, 1.0 / SAMPLE_RATE)
-    F = F/1000.0
-    N = N//2
-    return N, F[:N], Y[:N]/N
-
-def convert_to_g(y):
-    y_g = [v * const_g for v in y]
-    return y_g
-
-def convert_to_a(y):
-    y_a = [v / const_g for v in y]
-    return y_a
-
-
+from scipy.fft import fft, fftfreq
 
 
 class VibCharts:
-    def __init__(self, 
-            coleta = 14, 
-            test = 5, 
+    def __init__(self,
+            coleta = 14,
+            test = 5,
             Sensores = [1],
             frequence = 100000,
             path = "/media/eduardo/HDEduardoAnacleto/DADOS/Coleta"
@@ -60,12 +30,60 @@ class VibCharts:
         self.Time = []
         self.duration = 0
 
+        self.y_rms = [0,0,0]
+        self.y_std = [0,0,0]
+        self.y_min_vib = [0,0,0]
+        self.y_max_vib = [0,0,0]
+        self.y_max_dft = [0,0,0]
+
+
+    def create_directory(self, folder_name):
+        isExist = os.path.exists(self.path + str(self.coleta) + "/" + folder_name)
+        if not isExist:
+            os.makedirs(self.path + str(self.coleta) + "/" + folder_name)
+
+    def DFT(self, y):
+        Y = np.abs(fft(y))
+        N = len(Y)
+        freq = fftfreq(N, 1.0 / self.frequence)
+        N = N//2
+        return F[:N], Y[:N]
+
+    def DFT_normalized(self, y):
+        Y = np.abs(fft(y))
+        N = len(Y)
+        F = fftfreq(N, 1.0 / self.frequence)
+        F = F/1000.0
+        N = N//2
+        return N, F[:N], Y[:N]/N
+
+    def get_dft_max(self, y):
+        N, F, yf = self.DFT_normalized(y)
+        max_dft = np.max(yf)
+        return max_dft
+
+    def convert_to_g(self, y):
+        y_g = [v * self.const_g for v in y]
+        return y_g
+
+    def convert_to_a(self, y):
+        y_a = [v / self.const_g for v in y]
+        return y_a
+
+    def get_rms(self, y):
+        rms = 0
+        for v in y:
+            rms += v**2
+        rms = math.sqrt(rms / len(y))
+        return rms
+
+
 
     def getFirstSensorIndex(self):
         return self.Sensores[0]-1
 
     def getVibrations(self):
-        # Lear dados
+        # Read data
         text_file = open(self.path + str(self.coleta) + "/teste_" + str(self.test) + ".csv", "r" )
         #skip the first two lines
         if self.coleta != 12 or self.coleta != 11:
@@ -76,36 +94,62 @@ class VibCharts:
         for line in text_file:
             row_text = line.split(";")
             for s in self.Sensores:
-                if self.coleta != 12 or self.coleta != 11:
-                    self.Vibrations[s-1].append( float(row_text[s]))
-                else:
-                    self.Vibrations[s-1].append( float(row_text[s]) / self.const_g)
+                self.Vibrations[s-1].append( float(row_text[s]))
+
             self.numVibrations += 1
         text_file.close()
 
         # Duration in seconds 
         self.duration = self.numVibrations / self.frequence
 
-        # time
+        # time vector
         self.Time = np.linspace(0.0, self.duration, self.numVibrations, endpoint=False)
+
+
+    def getParameters(self):
+
+        self.create_directory("Tables")
+        self.getVibrations()
+
+        for sensor in self.Sensores:
+            self.y_rms[sensor-1] = self.get_rms(self.Vibrations[sensor-1])
+            self.y_std[sensor-1] = np.std(self.Vibrations[sensor-1])
+            self.y_min_vib[sensor-1] = np.min(self.Vibrations[sensor-1])
+            self.y_max_vib[sensor-1] = np.max(self.Vibrations[sensor-1])
+            self.y_max_dft[sensor-1] = self.get_dft_max(self.Vibrations[sensor-1])
+
+
+        with open(self.path + str(self.coleta) + "/Tables/info_" + str(self.unit) + "_coleta" + ".txt", "a") as f:
+            for sensor in self.Sensores:
+                f.write("{:2d}".format(self.coleta) +
+                        " {:2d};".format(self.test) +
+                        " {:1d};".format(sensor) +
+                        " {:10d};".format(self.numVibrations) +
+                        " {:10.8f};".format(self.duration) +
+                        " {:10.8f};".format(self.y_rms[sensor-1]) +
+                        " {:10.8f};".format(self.y_std[sensor-1]) +
+                        " {:10.8f};".format(self.y_min_vib[sensor-1]) +
+                        " {:10.8f};".format(self.y_max_vib[sensor-1]) +
+                        " {:10.8f};".format(self.y_max_dft[sensor-1]) +
+                        "\n")
 
 
     def plot_histogram(self):
 
         vib = [abs(v) for v in self.Vibrations[0]]
 
-        n, bins, patches = plt.hist(vib, 1000, density=False, facecolor='g')
+        n, bins, patches = plt.hist(vib, 1000, density = False, facecolor='g')
         #plt.xlabel('Smarts')
         #plt.ylabel('Probability')
         plt.title('Histogram')
         #plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
         #plt.xlim(40, 160)
         #plt.ylim(0, 0.03)
-        plt.grid(True)
+        plt.grid( True )
         plt.show()
 
     
-    def plotVibration(self):
+    def plotVibration( self ):
         
         if self.numParts == 0:
             if self.unit == 'a':
@@ -128,7 +172,7 @@ class VibCharts:
             for i in range(self.numParts):
                 if self.unit == 'a':
                     plt.plot( self.Time[self.parts[i][1]:self.parts[i][2]], 
-                              self.Vibrations[0][self.parts[i][1]:self.parts[i][2]], 'b' )
+                        self.Vibrations[0][self.parts[i][1]:self.parts[i][2]], 'b' )
                     plt.ylabel( "Amplitude (m/s^2)" )
 
                 elif self.unit == 'g':
@@ -220,11 +264,16 @@ class VibCharts:
             print(parts[i])
 
         self.parts = parts
+
+
         
 
 
-v = VibCharts(14, 1, [1])
-v.getVibrations()
-#v.plotVibration()
-v.vibrationParts()
-v.plotVibration()
+if __name__ == '__main__':
+
+    v = VibCharts(14, 1, [1])
+    v.getParameters()
+    #v.getVibrations()
+    #v.plotVibration()
+    #v.vibrationParts()
+    #v.plotVibration()
